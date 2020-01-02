f1ea5a547b96ca020066cd2595178a10ff9a3a47
Yong L <flyfly3760@gmail.com>
2020-01-02 16:47:51 -0500

    pls work 2

--- AB-bot.js
+++ AB-bot.js
@@ -46,23 +46,23 @@ client.on('message', message => {
     }
   }
   //random number command
-  if (command === '!rng') {
-    const minimum = Number(args[0]);
-  	const maximum = Number(args[1]);
-  	if(args.length !== 2) {
-  		return message.channel.send("Enter TWO numbers after the command");
-
-  	}
-  	else if(isNaN(minimum) || isNaN(maximum)) {
-  		return message.channel.send("Enter two NUMBERS after the command");
-  	}
-    else if(maximum <= minimum) {
-      return message.channel.send("The first number has to be smaller than the second")
-    } else {
-  		const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
-  		message.channel.send(`Your random number was ${randomNumber}`);
-  	}
-  }
+  // if (command === '!rng') {
+  //   const minimum = Number(args[0]);
+  // 	const maximum = Number(args[1]);
+  // 	if(args.length !== 2) {
+  // 		return message.channel.send("Enter TWO numbers after the command");
+  //
+  // 	}
+  // 	else if(isNaN(minimum) || isNaN(maximum)) {
+  // 		return message.channel.send("Enter two NUMBERS after the command");
+  // 	}
+  //   else if(maximum <= minimum) {
+  //     return message.channel.send("The first number has to be smaller than the second")
+  //   } else {
+  // 		const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+  // 		message.channel.send(`Your random number was ${randomNumber}`);
+  // 	}
+  // }
 
 
 
