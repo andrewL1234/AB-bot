1d30eef949728a5c88f798da8fa10a9ad22298fc
Yong L <flyfly3760@gmail.com>
2020-01-01 19:54:23 -0500

    Created the prefix of bot and updated commands

--- AB-bot.js
+++ AB-bot.js
@@ -8,9 +8,24 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-  if (message.content.toLowerCase() === 'hi') {
-    message.reply('helllo');
+  if(message.channel.type === "dm") return;
+	if(message.author.bot) return;
+
+  let prefix = "~";
+	let messageArray = message.content.split(" ");
+	let args = messageArray.slice(1);
+	let command = messageArray[0];
+
+  if(!command.startsWith(prefix)) return;
+
+  //reply command
+  if (command === 'say') {
+    message.channel.send(args.join(' '));
   }
+
+  // if (message.content.toLowerCase() === 'hi') {
+  //   message.reply('hello');
+  // }
 });
 
 
