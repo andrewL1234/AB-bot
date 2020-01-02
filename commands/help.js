10ba8aee419c4225241fa8fe30aae63e83f7c2b9
Yong L <flyfly3760@gmail.com>
2020-01-02 17:39:13 -0500

    efawefwef

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,5 @@
 const Discord = module.require("discord.js");
 const fs = module.require("fs");
-let jsfiles;
 const cmdDescriptions = {
 	"!box": "Opens a box",
 	"!help": "Bot help",
@@ -12,20 +11,16 @@ const cmdDescriptions = {
 	"!timer": "Set a timer",
 	"!userinfo": "Get the info of an user"
 }
-fs.readdir('./commands/', (err, files) => {
-	if (err) console.log(err);
 
-	jsfiles = files.filter(f => f.split(".").pop() === "js");
-})
 module.exports.run = (client, message, args) => {
 	const helpEmbed = new Discord.RichEmbed()
 	.setTitle("Bot Help")
 	.setColor("#3afbff")
 
 	let times = 0;
-	while(times < jsfiles.length) {
+	while(for item in Object.keys(cmdDescriptions)) {
 		let command = jsfiles[times].split(".")[0];
-		helpEmbed.addField(`**${command}**`, `\`${cmdDescriptions[command]}\``);
+		helpEmbed.addField(`**${item}**`, `${cmdDescriptions.item}`);
 		times++;
 	}
 
