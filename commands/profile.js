b683fd08f94fe89b0a44ceb2fbad02ed575d3202
Yong L <flyfly3760@gmail.com>
2020-01-02 17:51:28 -0500

    profile bad

--- commands/profile.js
+++ commands/profile.js
@@ -1,6 +1,6 @@
 module.exports.run = (client, message, args) => {
-message.channel.send("andew bad");
+	message.channel.send("andew bad");
 }
-module-.exports.help = {
+module.exports.help = {
 	name: "profile"
 };
