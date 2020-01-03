2ddcbe977ba815a6802e3a560e4ac03c9025ffe3
Brandon Liu <brandonliu2023@gmail.com>
2020-01-02 19:32:01 -0500

    test

--- commands/xp.js
+++ commands/xp.js
@@ -1,10 +1,10 @@
-const Discord = module.require("discord.js");
-const mongoose = require("mongoose")
-mongoose.connect('mongodb://localhost/local', {
-  useNewUrlParser: true,
-});
-const xp = require("xp.js");
-
-module.exports.run = (client, message, args) => {
-  
-}
+// const Discord = module.require("discord.js");
+// const mongoose = require("mongoose")
+// mongoose.connect('mongodb://localhost/local', {
+//   useNewUrlParser: true,
+// });
+// const xp = require("xp.js");
+//
+// module.exports.run = (client, message, args) => {
+//
+// }
