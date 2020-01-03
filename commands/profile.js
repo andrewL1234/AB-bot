e014817889cd922581743d586810585b52579d45
Brandon Liu <brandonliu2023@gmail.com>
2020-01-02 21:10:34 -0500

    maybe it works

--- commands/profile.js
+++ commands/profile.js
@@ -1,6 +1,6 @@
 const Discord = module.require("discord.js");
 const mongoose = require("mongoose")
-mongoose.connect('mongodb://127.0.0.1/xp', {
+mongoose.connect('mongodb+srv://${process.env.MUSERNAME}:${process.env.MPASSWORD}@ab-bot-wohsi.mongodb.net/xp', {
   useNewUrlParser: true,
 });
 const xp = require("../models/xp.js");
@@ -9,28 +9,28 @@ const Xp = mongoose.model('Xp', mySchema);
 module.exports.run = (client, message, args) => {
 
   const pEmbed = new Discord.RichEmbed()
-  .setTitle("Profile")
-  .setColor("#06172e")
-  .setFooter(`${message.author.username}`, message.author.displayAvatarURL);
+    .setTitle("Profile")
+    .setColor("#06172e")
+    .setFooter(`${message.author.username}`, message.author.displayAvatarURL);
 
-	xp.findOne({
-	  userID: message.author.id,
-	  serverID: message.guild.id
-	}, (err, res) => {
-	  if (err) console.log(err);
+  xp.findOne({
+    userID: message.author.id,
+    serverID: message.guild.id
+  }, (err, res) => {
+    if (err) console.log(err);
 
-	  if(!res) {
-	    pEmbed.addField("You do not appear to have any xp...");
-	  } else {
-			console.log(res)
-	    // pEmbed.addField(res.username, res.xp + "xp.");
+    if (!res) {
+      pEmbed.addField("You do not appear to have any xp...");
+    } else {
+      console.log(res)
+      // pEmbed.addField(res.username, res.xp + "xp.");
       pEmbed.addField(Xp.xp);
-	  }
+    }
 
-	})
-	message.channel.send(pEmbed)
+  })
+  message.channel.send(pEmbed)
 }
 //yeet f
 module.exports.help = {
-	name: "profile"
+  name: "profile"
 };
