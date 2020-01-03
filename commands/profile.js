1fcb3b067ddd6bf991b1048be22c253912511596
Brandon Liu <brandonliu2023@gmail.com>
2020-01-02 19:26:02 -0500

    xp v3?

--- commands/profile.js
+++ commands/profile.js
@@ -2,17 +2,31 @@ const Discord = module.require("discord.js");
 const mongoose = require("mongoose")
 mongoose.connect('mongodb://localhost/local', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
 });
+const xp = require("../models/xp.js");
 
 module.exports.run = (client, message, args) => {
 
   const pEmbed = new Discord.RichEmbed()
   .setTitle("Profile")
   .setColor("#06172e")
-  .setFooter(`${message.author.username}`, message.author.displayAvatarURL)
-  message.channel.send(pEmbed);
+  .setThumbnail(`${message.author.username}`, message.author.displayAvatarURL);
+
+xp.findOne({
+  userID: message.author.id,
+  serverID: message.guild.id
+}, (err, res) => {
+  if (err) console.log(err);
+
+  if(!res) {
+    pEmbed.addField("You do not appear to have any xp...");
+  } else {
+    pEmbed.addField(res.username, res.xp + "xp.")
+  }
 }
+
+  message.channel.send(pEmbed)
+})
 //yeet
 module.exports.help = {
 	name: "profile"
---
+++ models/xp.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const xpSchema = mongoose.Schema({
+  userID: String,
+  username: String,
+  serverID: String,
+  xp: Number
+});
+
+module.exports = mongoose.model("xp", coinSchema)
