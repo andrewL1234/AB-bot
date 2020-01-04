225a6bde2b7df257fc41c745e146e3b1c0fecf60
Brandon Liu <brandonliu2023@gmail.com>
2020-01-04 11:50:28 -0500

    yeet

--- commands/profile.js
+++ commands/profile.js
@@ -1,8 +1,8 @@
 const Discord = module.require("discord.js");
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://${process.env.MUSERNAME}:${process.env.MPASSWORD}@ab-bot-wohsi.mongodb.net/xp', {
-  useNewUrlParser: true,
-});
+// mongoose.connect('mongodb+srv://${process.env.MUSERNAME}:${process.env.MPASSWORD}@ab-bot-wohsi.mongodb.net/xp', {
+//   useNewUrlParser: true,
+// });
 
 
 
