55aa1d38829c038dc4dd8a4601b88b0f1148c436
Brandon Liu <brandonliu2023@gmail.com>
2020-01-04 11:45:47 -0500

    maybe

--- commands/profile.js
+++ commands/profile.js
@@ -3,8 +3,25 @@ const mongoose = require("mongoose")
 mongoose.connect('mongodb+srv://${process.env.MUSERNAME}:${process.env.MPASSWORD}@ab-bot-wohsi.mongodb.net/xp', {
   useNewUrlParser: true,
 });
+var id = message.author.id;
+var usne = message.author.username;
+var server = message.guild.id;
 
- const xp = mongoose.model('xp');
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true,
+});
+const Schema = mongoose.Schema;
+
+
+const xpSchema = new Schema({
+  userID: String,
+  username: String,
+  serverID: String,
+  xp: Number
+});
+
+const xp = mongoose.model('xp', xpSchema);
 
 module.exports.run = (client, message, args) => {
 
@@ -13,21 +30,29 @@ module.exports.run = (client, message, args) => {
     .setColor("#06172e")
     .setFooter(`${message.author.username}`, message.author.displayAvatarURL);
 
-  xp.findOne({
-    userID: message.author.id,
-    serverID: message.guild.id
-  }, (err, res) => {
-    if (err) console.log(err);
+    var xpp = new xp(
+      //{userID: id},
+      //{username: usne},
+      //{serverID: server},
+      {xp: 12})
+    console.log(xpp.xp);
 
-    if (!res) {
-      pEmbed.addField("You do not appear to have any xp...");
-    } else {
-      console.log(res)
-      pEmbed.addField(res.username, res.xp + "xp.");
-      // pEmbed.addField(Xp.xp);
-    }
-
-  })
+    pEmbed.addField(xpp);
+  // xp.findOne({
+  //   userID: message.author.id,
+  //   serverID: message.guild.id
+  // }, (err, res) => {
+  //   if (err) console.log(err);
+  //
+  //   if (!res) {
+  //     pEmbed.addField("You do not appear to have any xp...");
+  //   } else {
+  //     console.log(res)
+  //     pEmbed.addField(res.username, res.xp + "xp.");
+  //     // pEmbed.addField(Xp.xp);
+  //   }
+  //
+  // })
   message.channel.send(pEmbed)
 }
 //yeet f
