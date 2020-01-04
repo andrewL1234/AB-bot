704d9cc8aa6a23bcee32fc84aae98a48e3003707
Yong L <flyfly3760@gmail.com>
2020-01-04 13:21:13 -0500

    something happened

--- AB-bot.js
+++ AB-bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const xpInfo = require('xp.json');
+const xpInfo = require('./xp.json');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const dbs = process.env.MONGODB_URL
